8921d0fe19d6f1d4e3f41c595d5ca5d5d49120a0
MasonM <Mason.P.Marinko@gmail.com>
2020-05-27 15:45:02 -0600

    Updated so tasks when late or due soon go red or yellow

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,22 @@ $("#modalDueDate").datepicker({
 var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
+
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
-  var taskP = $("<p>")
+  
+  
+    var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -149,7 +155,6 @@ $(".list-group").on("change", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
-    .trim();
 
   // get the parent ul's id attribute
   var status = $(this)
@@ -173,6 +178,9 @@ $(".list-group").on("change", "input[type='text']", function () {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 })
 
 // made todo boxes clickable
@@ -258,6 +266,24 @@ $("#remove-tasks").on("click", function () {
   }
   saveTasks();
 });
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date= $(taskEl).find("span").text().trim();
+
+    // convert to moment object at 5:00PM
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-danger list-group-item-warning");
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days"))<= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 // load tasks for the first time
 loadTasks();
